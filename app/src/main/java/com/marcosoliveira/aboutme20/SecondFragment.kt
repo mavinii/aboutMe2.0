24ad8ea6ae6a13a10ba648ecb3d3b22cbb277fc0
mavinii <pg.marcosoliveira@icloud.com>
2022-03-01 11:33:54 +0000

    - Starting the Education screen using cards.

--- app/src/main/java/com/marcosoliveira/aboutme20/SecondFragment.kt
+++ app/src/main/java/com/marcosoliveira/aboutme20/SecondFragment.kt
@@ -5,9 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
-                               // TODO: This is the variable to call the screen
-class SecondFragment : Fragment(R.layout.fragment_second) {
+                               // TODO: This is the variable to call the screen ...Fragment("R.layout.fragment_second")
+class SecondFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
